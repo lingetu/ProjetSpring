e2a7687e184954310061978b48600b519ce42e31
averrani <aris.issad@etu.univ-amu.fr>
2024-05-28 15:22:58 +0200

    push

--- angular-QRCODE-website/src/app/planning/planning.component.ts
+++ angular-QRCODE-website/src/app/planning/planning.component.ts
@@ -38,14 +38,24 @@ export class PlanningComponent implements OnInit {
   }
 
   onSubmit() {
-    if (this.newEventForm.invalid) {
-      return;
-    }
+  const agendaId = this.newEventForm.get('agendaId').value;
+  const dateDebut = this.newEventForm.get('dateDebut').value;
+  const dateFin = this.newEventForm.get('dateFin').value;
+  const type = this.newEventForm.get('type').value;
+  const typeEvenement = this.newEventForm.get('typeEvenement').value;
 
-    const event = {
-      ...this.newEventForm.value,
-      utilisateurId: this.guest.id
-    };
+  const event = {
+    agenda_id: agendaId,
+    dateDebut: dateDebut,
+    dateFin: dateFin,
+    type: type,
+    typeEvenement: typeEvenement,
+    utilisateurId: this.guest.id
+  };
+
+  // Vous pouvez maintenant utiliser event2 comme nécessaire
+
+    console.log("Event to add: ", event);
 
     this.planningService.addPlanning(event).subscribe(response => {
       console.log("Event added successfully", response);
